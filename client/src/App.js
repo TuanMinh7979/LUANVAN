719d9330101ccf36b429d8fdf3a7411a52534f2a
Theas <congphongkiemsi@gmail.com>
2022-10-02 15:26:41 +0700

    QA 2/10 init

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { jobs } from './dummyData.js/jobs';
+import { jobs } from './dummyData/jobs';
 function App() {
   return (
     <div className="App">
