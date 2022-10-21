02230de277b7578b8435641e77311e8026f2e1d5
tuanminh2 <aroi671620@gmail.com>
2022-10-21 09:22:35 +0700

    test err post

---
+++ client/src/components/other/Other.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Form from "../../components/other/yupValidate/Form";
+import axios from "axios";
+function Other() {
+  // DUNG CAI NAY THI T TRA VE MA LOI 400 THI PHAI TRY CATCH
+  const postData = async () => {
+    try {
+      let resp = await axios.post("http://localhost:8800/api/auth/login", {
+        username: "admi12n",
+        password: "1234",
+      });
+      console.log("YEP", resp);
+    } catch (e) {
+      console.log(e.response.data);
+    }
+  };
+
+  // DUNG CAI NAY THI T TRA VE MA LOI 200 KHONG PHAI TRY CATCH
+  const postData1 = async () => {
+    let resp = await axios.post("http://localhost:8800/api/auth/login", {
+      username: "admi12n",
+      password: "1234",
+    });
+    console.log("RES", resp);
+    if (resp.status !== 200) {
+      console.log("FAILED", err);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={postData1}>Send data fake</button>
+    </div>
+  );
+}
+export default Other;
