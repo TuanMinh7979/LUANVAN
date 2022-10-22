79170fbd7372f04ffb0b7250ab0a2a4c3475d1d7
Theas <congphongkiemsi@gmail.com>
2022-10-22 14:22:15 +0700

    index update 22/10 QA

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,215 +2,217 @@ import { Box, Button, InputAdornment, TextField, Typography, FormControl, Select
 import Image from "mui-image";
 import { useState } from "react";
 import FeatureCard from "./FeatureCard";
+import JobList from "./JobList";
 
 function Home() {
-    const [displayFilterBox, setDisplayFilterBox] = useState("none")
-    return (
-        <>
-            <Box
-                sx={{
-                    background: 'linear-gradient(to left, #8cebc84a, transparent)',
-                    mb: 0,
-                    p: 6
-                }}
-            >
-                <Box
-                    display="flex"
-                >
-                    <Box sx={{
-                        mt: "7rem",
-                        flexDirection: "column"
-                    }}>
-                        <Typography variant="h4" fontWeight="550" gutterBottom>
+  const [displayFilterBox, setDisplayFilterBox] = useState("none")
+  return (
+    <>
+      <Box
+        sx={{
+          background: 'linear-gradient(to left, #8cebc84a, transparent)',
+          mb: 0,
+          p: 6
+        }}
+      >
+        <Box
+          display="flex"
+        >
+          <Box sx={{
+            mt: "7rem",
+            flexDirection: "column"
+          }}>
+            <Typography variant="h4" fontWeight="550" gutterBottom>
                             Tìm kiếm công việc phù hợp với bạn
-                        </Typography>
-                        <Box
-                            sx={{
-                                display: "flex",
-                                flexWrap: "wrap",
-                            }}
-                        >
-                            <TextField
-                                onFocus={() => {
-                                    setDisplayFilterBox("block")
-                                }}
-                                size="small"
-                                sx={{ width: "70%" }}
-                                id="outlined-start-adornment"
-                                color="success"
-                                label="Nhập tên công việc, ngành nghề bạn muốn ứng tuyển"
-                                inputProps={{
-                                    startAdornment: <InputAdornment position="start">kg</InputAdornment>,
-                                }}
-                            />
-                            <Button
-                                size="small"
-                                sx={{
-                                    ml: 2
-                                }}
-                                variant="contained"
-                                color="success"
-                            >Tìm việc ngay</Button>
-                            <Box width="30rem">
-                                <Box display={displayFilterBox}>
-                                    <Box display="flex" justifyContent="space-between" sx={{ my: 2 }}>
-                                        <Typography variant="h6" fontSize="1.1rem" gutterBottom>
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "wrap"
+              }}
+            >
+              <TextField
+                onFocus={() => {
+                  setDisplayFilterBox("block")
+                }}
+                size="small"
+                sx={{ width: "70%" }}
+                id="outlined-start-adornment"
+                color="success"
+                label="Nhập tên công việc, ngành nghề bạn muốn ứng tuyển"
+                inputProps={{
+                  startAdornment: <InputAdornment position="start">kg</InputAdornment>
+                }}
+              />
+              <Button
+                size="small"
+                sx={{
+                  ml: 2
+                }}
+                variant="contained"
+                color="success"
+              >Tìm việc ngay</Button>
+              <Box width="30rem">
+                <Box display={displayFilterBox}>
+                  <Box display="flex" justifyContent="space-between" sx={{ my: 2 }}>
+                    <Typography variant="h6" fontSize="1.1rem" gutterBottom>
                                             Tìm kiếm nâng cao
-                                        </Typography>
-                                        <Button size="small" sx={{ p: 0 }} onClick={() => setDisplayFilterBox("none")}>Thu gọn</Button>
-                                    </Box>
-                                    <Box display="flex" flexWrap="wrap" width="100%">
+                    </Typography>
+                    <Button size="small" sx={{ p: 0 }} onClick={() => setDisplayFilterBox("none")}>Thu gọn</Button>
+                  </Box>
+                  <Box display="flex" flexWrap="wrap" width="100%">
 
-                                        <FormControl
-                                            size="small"
-                                            sx={{ width: "40%", mb: 1, mr: 2 }}
-                                        >
-                                            <InputLabel id="demo-simple-select-label">Ngành nghề</InputLabel>
-                                            <Select
-                                                label="Ngành nghề"
-                                            >
-                                                <MenuItem value={10}>Ten</MenuItem>
-                                                <MenuItem value={20}>Twenty</MenuItem>
-                                                <MenuItem value={30}>Thirty</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                        <FormControl
-                                            size="small"
-                                            sx={{ width: "40%", mb: 1, mr: 2 }}
-                                        >
-                                            <InputLabel id="demo-simple-select-label">Cấp bậc</InputLabel>
-                                            <Select
-                                                label="Cấp bậc"
-                                            >
-                                                <MenuItem value={10}>Ten</MenuItem>
-                                                <MenuItem value={20}>Twenty</MenuItem>
-                                                <MenuItem value={30}>Thirty</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                        <FormControl
-                                            size="small"
-                                            sx={{ width: "40%", mb: 1, mr: 2 }}
-                                        >
-                                            <InputLabel id="demo-simple-select-label">Mức lương</InputLabel>
-                                            <Select
-                                                label="Mức lương"
-                                            >
-                                                <MenuItem value={10}>Ten</MenuItem>
-                                                <MenuItem value={20}>Twenty</MenuItem>
-                                                <MenuItem value={30}>Thirty</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                        <FormControl
-                                            size="small"
-                                            sx={{ width: "40%", mb: 1, mr: 2 }}
-                                        >
-                                            <InputLabel id="demo-simple-select-label">Địa điểm công ty</InputLabel>
-                                            <Select
-                                                label="Địa điểm công ty"
-                                            >
-                                                <MenuItem value={10}>Ten</MenuItem>
-                                                <MenuItem value={20}>Twenty</MenuItem>
-                                                <MenuItem value={30}>Thirty</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                    </Box>
-                                </Box>
-                                <Box
-                                    sx={{
-                                        mt: "1.5rem",
-                                    }}
-                                >
-                                    <Typography variant="h6" fontWeight={550}>
-                                        Các công ty tuyển dụng hàng đầu
-                                    </Typography>
-                                    <Box
-                                        sx={{
-                                            mt: "1rem",
-                                            display: "flex"
-                                        }}
-                                    >
-                                        <Image
-                                            sx={{
-                                                maxWidth: "58px",
-                                                maxHeight: "30px",
-                                                mr: 4
-                                            }}
-                                            src="https://www.topcv.vn/v4/image/welcome/companies/onemoutn.png"
-                                            fit="cover"
-                                            duration={0}
-                                        />
-                                        <Image
-                                            sx={{
-                                                maxWidth: "58px",
-                                                maxHeight: "30px",
-                                                mr: 4
-                                            }}
-                                            src="https://www.topcv.vn/v4/image/welcome/companies/teachcombank.png"
-                                            fit="cover"
-                                            duration={0}
-                                        />
-                                        <Image
-                                            sx={{
-                                                maxWidth: "58px",
-                                                maxHeight: "30px",
-                                                mr: 4
-                                            }}
-                                            src="https://www.topcv.vn/v4/image/welcome/companies/viettel.png"
-                                            fit="cover"
-                                            duration={0}
-                                        />
-                                        <Image
-                                            sx={{
-                                                maxWidth: "58px",
-                                                maxHeight: "30px",
-                                                mr: 4
-                                            }}
-                                            src="https://www.topcv.vn/v4/image/welcome/companies/tiki.png"
-                                            fit="cover"
-                                            duration={0}
-                                        />
-                                        <Image
-                                            sx={{
-                                                maxWidth: "58px",
-                                                maxHeight: "30px",
-                                                mr: 4
-                                            }}
-                                            src="https://www.topcv.vn/v4/image/welcome/companies/fpt.png"
-                                            fit="cover"
-                                            duration={0}
-                                        />
-                                    </Box>
-                                </Box>
-                            </Box>
-                        </Box>
-                    </Box>
-                    <Image
-                        src="https://www.topcv.vn/v4/image/welcome/image_topcv.png?v=1.0.0"
-                        fit="cover"
-                        height="50%"
-                        width="50%"
-                        duration={0}
-                    />
+                    <FormControl
+                      size="small"
+                      sx={{ width: "40%", mb: 1, mr: 2 }}
+                    >
+                      <InputLabel id="demo-simple-select-label">Ngành nghề</InputLabel>
+                      <Select
+                        label="Ngành nghề"
+                      >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
+                    <FormControl
+                      size="small"
+                      sx={{ width: "40%", mb: 1, mr: 2 }}
+                    >
+                      <InputLabel id="demo-simple-select-label">Cấp bậc</InputLabel>
+                      <Select
+                        label="Cấp bậc"
+                      >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
+                    <FormControl
+                      size="small"
+                      sx={{ width: "40%", mb: 1, mr: 2 }}
+                    >
+                      <InputLabel id="demo-simple-select-label">Mức lương</InputLabel>
+                      <Select
+                        label="Mức lương"
+                      >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
+                    <FormControl
+                      size="small"
+                      sx={{ width: "40%", mb: 1, mr: 2 }}
+                    >
+                      <InputLabel id="demo-simple-select-label">Địa điểm công ty</InputLabel>
+                      <Select
+                        label="Địa điểm công ty"
+                      >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
                 </Box>
                 <Box
-                    sx={{ display: "flex", justifyContent: "space-between", mt: 4, px: 4 }}>
-                    <FeatureCard
-                        maxHeight="262px"
-                        maxWidth="42%"
-                        title="Tạo CV online ấn tượng"
-                        subTitle="ViecLamNhanh hiện có 50+ mẫu CV chuyên nghiệp, độc đáo phù hợp với mọi ngành nghề"
-                        imageLink="https://www.topcv.vn/v4/image/welcome/mau_cv.png?v=1.0.0"
-                        buttonTitle="Tạo CV ngay" />
-                    <FeatureCard
-                        maxHeight="262px"
-                        maxWidth="42%"
-                        title="Tạo CV online ấn tượng"
-                        subTitle="ViecLamNhanh hiện có 50+ mẫu CV chuyên nghiệp, độc đáo phù hợp với mọi ngành nghề"
-                        imageLink="https://www.topcv.vn/v4/image/welcome/mau_cv.png?v=1.0.0"
-                        buttonTitle="Tạo CV ngay" />
+                  sx={{
+                    mt: "1.5rem"
+                  }}
+                >
+                  <Typography variant="h6" fontWeight={550}>
+                                        Các công ty tuyển dụng hàng đầu
+                  </Typography>
+                  <Box
+                    sx={{
+                      mt: "1rem",
+                      display: "flex"
+                    }}
+                  >
+                    <Image
+                      sx={{
+                        maxWidth: "58px",
+                        maxHeight: "30px",
+                        mr: 4
+                      }}
+                      src="https://www.topcv.vn/v4/image/welcome/companies/onemoutn.png"
+                      fit="cover"
+                      duration={0}
+                    />
+                    <Image
+                      sx={{
+                        maxWidth: "58px",
+                        maxHeight: "30px",
+                        mr: 4
+                      }}
+                      src="https://www.topcv.vn/v4/image/welcome/companies/teachcombank.png"
+                      fit="cover"
+                      duration={0}
+                    />
+                    <Image
+                      sx={{
+                        maxWidth: "58px",
+                        maxHeight: "30px",
+                        mr: 4
+                      }}
+                      src="https://www.topcv.vn/v4/image/welcome/companies/viettel.png"
+                      fit="cover"
+                      duration={0}
+                    />
+                    <Image
+                      sx={{
+                        maxWidth: "58px",
+                        maxHeight: "30px",
+                        mr: 4
+                      }}
+                      src="https://www.topcv.vn/v4/image/welcome/companies/tiki.png"
+                      fit="cover"
+                      duration={0}
+                    />
+                    <Image
+                      sx={{
+                        maxWidth: "58px",
+                        maxHeight: "30px",
+                        mr: 4
+                      }}
+                      src="https://www.topcv.vn/v4/image/welcome/companies/fpt.png"
+                      fit="cover"
+                      duration={0}
+                    />
+                  </Box>
                 </Box>
-            </Box>                               
-        </>
-    )
+              </Box>
+            </Box>
+          </Box>
+          <Image
+            src="https://www.topcv.vn/v4/image/welcome/image_topcv.png?v=1.0.0"
+            fit="cover"
+            height="50%"
+            width="50%"
+            duration={0}
+          />
+        </Box>
+        <Box
+          sx={{ display: "flex", justifyContent: "space-between", my: 4, px: 4 }}>
+          <FeatureCard
+            maxHeight="262px"
+            maxWidth="42%"
+            title="Tạo CV online ấn tượng"
+            subTitle="ViecLamNhanh hiện có 50+ mẫu CV chuyên nghiệp, độc đáo phù hợp với mọi ngành nghề"
+            imageLink="https://www.topcv.vn/v4/image/welcome/mau_cv.png?v=1.0.0"
+            buttonTitle="Tạo CV ngay" />
+          <FeatureCard
+            maxHeight="262px"
+            maxWidth="42%"
+            title="Tạo CV online ấn tượng"
+            subTitle="ViecLamNhanh hiện có 50+ mẫu CV chuyên nghiệp, độc đáo phù hợp với mọi ngành nghề"
+            imageLink="https://www.topcv.vn/v4/image/welcome/mau_cv.png?v=1.0.0"
+            buttonTitle="Tạo CV ngay" />
+        </Box>
+        <JobList />
+      </Box>
+    </>
+  )
 }
 export default Home;
--- client/src/components/JobCard.js
+++ client/src/components/JobCard.js
@@ -1,7 +1,55 @@
-function JobCard(){
-    return(<>
-        
-    </>)
-}
+import { Paper, Box, Typography, Grid, Card, CardActionArea, IconButton } from "@mui/material";
+import Image from "mui-image";
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+export default function JobCard({ jobTitle, companyLogo, companyName, salary, location }) {
+  function titleCut(jobTitle) {
 
-export default JobCard;
+  }
+  const style1 = {
+    background: "#EFEFEF",
+    padding: "3px",
+    borderRadius: "10px",
+    mr: 2
+  }
+  return (
+    <>
+      <Card sx={{ boxShadow: "-1px 1px 4px rgb(0 0 0 / 20%)" }}>
+        <CardActionArea>
+          <Box sx={{ display: "flex", padding: "10px", justifyContent: "space-around" }}>
+            <Grid container sx={{ width: "10%" }}>
+              <Grid xs="12">
+                <Image
+                  src={companyLogo}
+                  fit="contain"
+                  width="100%"
+                  height="100%"
+                  duration="0"
+                />
+              </Grid>
+            </Grid>
+            <Box>
+              <Typography fontWeight="600" fontSize="18px" variant="h6">
+                {jobTitle}
+              </Typography>
+              <Typography fontWeight="300" fontSize="20px" variant="h5">
+                {companyName}
+              </Typography>
+            </Box>
+            <IconButton>
+              <FavoriteBorderOutlinedIcon />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: "flex", padding: "10px", width: "50%" }}>
+            <Typography sx={style1}>
+              {salary}
+            </Typography>
+            <Typography sx= {style1}>
+              {location}
+            </Typography>
+          </Box>
+        </CardActionArea>
+      </Card>
+    </>
+  )
+}
