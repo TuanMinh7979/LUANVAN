6f2f9412987aa9e1bbed53c3e5a65776fbc3a243
TuanMinh7979 <minhtuanto179@gmail.com>
2022-10-23 19:57:31 +0700

    change user api

--- server/src/models/Applicant.js
+++ server/src/models/Applicant.js
@@ -3,13 +3,18 @@ const { Schema } = mongoose;
 
 const ApplicantSchema = new Schema(
   {
+    email: { type: String },
+    name: { type: String },
+    address: { type: String },
+    avatar: { type: String },
     user_id: { type: String, required: true },
     experience: { type: String },
     education: { type: String },
     skills: { type: [String] },
     resume: { type: String },
     phone: { type: String },
-  }
+  },
+  { timestamps: true }
   //createAt and updateAt
 );
 
--- server/src/models/Rec.js
+++ server/src/models/Rec.js
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const RecSchema = new Schema({
-  user_id: { type: String },
-  phone: { type: String },
-  company_name: { type: String },
-});
+const RecSchema = new Schema(
+  {
+    email: { type: String },
+    name: { type: String },
+    address: { type: String },
+    avatar: { type: String },
+    user_id: { type: String },
+    phone: { type: String },
+    company_name: { type: String },
+  },
+  { timestamps: true }
+);
 
 const Rec = mongoose.model("recs", RecSchema);
 export default Rec;
