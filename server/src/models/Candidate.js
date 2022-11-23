4be4a506f134636a619ba82ccda539217ebc0c2e
tuanminh2 <aroi671620@gmail.com>
2022-11-23 11:28:47 +0700

    add profile in model

--- server/src/models/Candidate.js
+++ server/src/models/Candidate.js
@@ -1,6 +1,21 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
-
+const ProfileSchema = mongoose.Schema({
+  age: { type: Number },
+  education: { type: String },
+  major: {
+    type: String,
+  },
+  skills: { type: [{}] },
+  location: {
+    type: String,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+  },
+  experience: { type: [{}] },
+});
 const CandidateSchema = new Schema(
   {
     email: { type: String },
@@ -8,11 +23,9 @@ const CandidateSchema = new Schema(
     address: { type: String },
     avatar: { type: String },
     userId: { type: String, required: true, unique: true },
-    experience: { type: String },
-    education: { type: String },
-    skills: { type: [String] },
     resume: { type: String },
     phone: { type: String },
+    profile: { type: ProfileSchema },
     saveJobs: {
       type: [String],
     },
