917ab830836ed2073d33f4b96d7ba12f6dd66efd
Theas <congphongkiemsi@gmail.com>
2022-12-12 22:53:29 +0700

    fix bug

--- client/src/components/CV/CV3.js
+++ client/src/components/CV/CV3.js
@@ -224,7 +224,9 @@ export default function CV3({ editable, data, setPrint, print, setCVDATA }) {
                                         display: 'flex',
                                         alignItems: 'center',
                                         mb: 1,
-                                        width: "40%"
+                                        width: "100%",
+                                        flexWrap: "wrap"
+                                        
                                     }}
                                 >
                                     <CalendarMonthIcon fontSize="small" sx={{ mr: 1 }} />
@@ -235,7 +237,8 @@ export default function CV3({ editable, data, setPrint, print, setCVDATA }) {
                                         display: 'flex',
                                         alignItems: 'center',
                                         mb: 1,
-                                        width: "40%"
+                                        width: "100%",
+                                        overflow: "hidden"
                                     }}
                                 >
                                     <PhoneIcon fontSize="small" sx={{ mr: 1 }} />
@@ -246,7 +249,8 @@ export default function CV3({ editable, data, setPrint, print, setCVDATA }) {
                                         display: 'flex',
                                         alignItems: 'center',
                                         mb: 1,
-                                        width: "40%"
+                                        width: "100%",
+                                        overflow: "hidden"
                                     }}
                                 >
                                     <MailIcon fontSize="small" sx={{ mr: 1 }} />
@@ -257,7 +261,8 @@ export default function CV3({ editable, data, setPrint, print, setCVDATA }) {
                                         display: 'flex',
                                         alignItems: 'center',
                                         mb: 1,
-                                        width: "40%"
+                                        width: "100%",
+                                        overflow: "hidden"
                                     }}
                                 >
                                     <FacebookIcon fontSize="small" sx={{ mr: 1 }} />
@@ -268,7 +273,7 @@ export default function CV3({ editable, data, setPrint, print, setCVDATA }) {
                                         display: 'flex',
                                         alignItems: 'center',
                                         mb: 1,
-                                        width: "40%"
+                                        width: "100%"
                                     }}
                                 >
                                     <LocationOnIcon fontSize="small" sx={{ mr: 1 }} />
