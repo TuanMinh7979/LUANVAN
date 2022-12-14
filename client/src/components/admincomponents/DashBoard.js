7321663b137ef3442befc793af34a2848899941b
Theas <congphongkiemsi@gmail.com>
2022-12-14 23:00:12 +0700

    ok

--- client/src/components/admincomponents/DashBoard.js
+++ client/src/components/admincomponents/DashBoard.js
@@ -1,21 +1,40 @@
-import { Box, Grid, MenuItem } from '@mui/material/'
-import { styled } from '@mui/system';
-const CustomMenuItem = styled(MenuItem)(({ theme }) => ({
-    "&:hover": {
-        color: theme.palette.success.light,
-        "& .MuiListItemIcon-root": {
-            color: theme.palette.success.light,
-        },
-    },
-}));
+import { Box,  Grid, MenuItem, MenuList, ListItemIcon, ListItemText, Paper } from '@mui/material/'
+import { styled, alpha, createTheme } from "@mui/material/styles";
+
 function SideBar() {
+    const CustomMenuItem = styled(MenuItem)(({ theme }) => ({
+        "&:hover": {
+            color: theme.palette.success.light,
+            "& .MuiListItemIcon-root": {
+                color: theme.palette.success.light,
+            },
+        },
+    }));
     return (
         <Box
             sx={{
                 width: "100%"
             }}
+            component={Paper}
         >
+            <MenuList>
+                <CustomMenuItem
+                    // sx={{
+                    //     color: activeItem == 0 ? theme.palette.success.main : "",
+                    // }}
 
+                >
+                    {/* <ListItemIcon sx={{ py: 2 }}>
+                        <EditIcon
+                            fontSize="small"
+                            sx={{
+                                color: activeItem == 0 ? theme.palette.success.main : "",
+                            }}
+                        />
+                    </ListItemIcon> */}
+                    <ListItemText>Đăng tin tuyển dụng mới</ListItemText>
+                </CustomMenuItem>
+            </MenuList>
         </Box>
     )
 }
@@ -32,6 +51,7 @@ export default function DashBoard() {
                 item
                 container
                 xs={4}>
+                    <SideBar />
             </Grid>
         </Grid>
     )
