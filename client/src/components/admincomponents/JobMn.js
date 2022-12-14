8989463ea9d729660339703cff219ae2cdfaf3ec
Theas <congphongkiemsi@gmail.com>
2022-12-15 00:28:12 +0700

    OK

--- client/src/components/admincomponents/JobMn.js
+++ client/src/components/admincomponents/JobMn.js
@@ -1,10 +1,19 @@
 import Grid from '@mui/material/Grid'
 import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Typography, createTheme, Paper, Select, MenuItem, Button } from '@mui/material'
 import useFetch from '../../hooks/useFetch'
+import Loading from "../Loading"
+import { useEffect, useState } from 'react'
 export default function JobMn() {
   const theme = createTheme()
   const status = ["Mới", "Đã duyệt"]
-  const {data, setData, loading} = useFetch("/jobpost")
+  
+  const { data, setData, loading } = useFetch("/jobpost/all")
+  const deleteItem = function(index){
+    data.splice(index,1)
+    let temp = data
+    setData(temp)
+    console.log(data)
+  }
   return (
     <>
       <Grid container spacing={0}>
@@ -12,7 +21,8 @@ export default function JobMn() {
           item
           xs={12}
         >
-          <TableContainer
+          {loading ? <Loading /> : <TableContainer
+            sx={{maxHeight: 500}}
             component={Paper}
           >
             <Table>
@@ -24,40 +34,44 @@ export default function JobMn() {
                 <TableRow sx={{ fontWeight: 600 }}>
                   <TableCell><Typography variant="body2" fontWeight={600} sx={{ color: "white" }}>Tên công việc</Typography></TableCell>
                   <TableCell><Typography variant="body2" fontWeight={600} sx={{ color: "white" }}>Tên công ty</Typography></TableCell>
-                  <TableCell><Typography variant="body2" fontWeight={600} sx={{ color: "white" }}>Ngày hết hạn</Typography></TableCell>
+                  <TableCell><Typography variant="body2" fontWeight={600} sx={{ color: "white", minWidth: "150px" }}>Ngày hết hạn</Typography></TableCell>
                   <TableCell><Typography variant="body2" fontWeight={600} sx={{ color: "white" }}>Địa chỉ</Typography></TableCell>
                   <TableCell></TableCell>
                   <TableCell></TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                <TableRow>
-                  <TableCell>TEST TEST ETSSTSytYTYTYTUY</TableCell>
-                  <TableCell>TEST TEST ETSSTSytYTYTYTUY</TableCell>
-                  <TableCell>TEST TEST ETSSTSytYTYTYTUY</TableCell>
-                  <TableCell>TEST TEST ETSSTSytYTYTYTUY</TableCell> 
-                  <TableCell sx={{minWidth: "150px"}}>
-                    <Select
-                      defaultValue={status[1]}
-                      size="small"
-                      onChange={(e) => handleStatusChange(e, 2)}
-                    >
-                      {status.map((item) => {
-                        return (
-                          <MenuItem value={item}>{item}</MenuItem>
-                        )
-                      })}
-                    </Select>
-                  </TableCell>
-                  <TableCell>
-                    <Button variant="text" color="error">
-                      Xóa
-                    </Button>
-                  </TableCell>
-                </TableRow>
+                {data.map((value, index) => (
+                  <TableRow>
+                    <TableCell>{value.title}</TableCell>
+                    <TableCell>{value.companyId.name}</TableCell>
+                    <TableCell>{new Date(value.endDate).toLocaleDateString()}</TableCell>
+                    <TableCell>{value.fullAddress}</TableCell>
+                    <TableCell sx={{ minWidth: "150px" }}>
+                      <Select
+                        defaultValue={status[1]}
+                        size="small"
+                        onChange={(e) => handleStatusChange(e, 2)}
+                      >
+                        {status.map((item) => {
+                          return (
+                            <MenuItem value={item}>{item}</MenuItem>
+                          )
+                        })}
+                      </Select>
+                    </TableCell>
+                    <TableCell>
+                      <Button variant="text" color="error" onClick={()=>{
+                        deleteItem(index)
+                      }}>
+                        Xóa
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
               </TableBody>
             </Table>
-          </TableContainer>
+          </TableContainer>}
         </Grid>
       </Grid>
     </>
